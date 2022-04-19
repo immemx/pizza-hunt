1687479d3200ffee3ddf9a3d6fda484fc6081371
immemx <eayresm4@gmail.com>
2022-04-19 17:07:33 -0400

    comment-model

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { addComment, removeComment } = require('../../controllers/comment-controller');
+
+// /api/comments/<pizzaid>
+router.route('/:pizzaId').post(addComment);
+
+// /api/comments/<pizzaid>/<commentId>
+router.route('/:pizzaId/:commentId').delete(removeComment)
+
+
+module.exports = router; 
